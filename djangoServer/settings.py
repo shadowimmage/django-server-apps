be9cf4f7742ff48d37dcc6fc3f4aa402fcd87d7a
Chase Sawyer <shadowimmage@gmail.com>
2017-11-19 17:50:27 -0800

    fixed settings for production

--- djangoServer/settings.py
+++ djangoServer/settings.py
@@ -73,7 +73,7 @@ DATABASES['default']['TEST'] = {'NAME': DATABASES['default']['NAME']}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
